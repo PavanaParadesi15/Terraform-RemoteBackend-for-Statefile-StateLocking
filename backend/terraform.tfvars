bd8dd9ec87e75326fb4373301c33b688d0fb881e
Pavana Paradesi <pp318@student.le.ac.uk>
2025-02-10 16:40:42 +0530

    added code

--- backend/terraform.tfvars
+++ backend/terraform.tfvars
@@ -1,3 +1,3 @@
-region = ap-south-1
+region = "ap-south-1"
 bucket_name = "terraform-eks-state-bucket-pavana"
 dynamodb_table_name = "terraform-eks-state-locks"
---
+++ modules/vpc/main.tf
@@ -0,0 +1,127 @@
+// Create VPC resource
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name                                           = "${var.cluster_name}-vpc"
+    "kubernetes.io/cluster/${var.cluster_name}"    = "shared"
+  }
+}
+
+
+// Create private subnet
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name                                           = "${var.cluster_name}-private-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}"    = "shared"
+    "kubernetes.io/role/internal-elb"              = "1"
+  }
+}
+
+
+// Public Subnet
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                           = "${var.cluster_name}-public-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}"    = "shared"
+    "kubernetes.io/role/elb"                       = "1"
+  }
+}
+
+
+// ----------------------------------------------------------------
+// Create  Internet Gateway for public subnet
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.cluster_name}-igw"
+  }
+}
+
+
+// Create Route table for Public subnet and public traffic
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-public"
+  }
+}
+
+
+// Associate Route Table with the Public Subnet
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+// ----------------------------------------------------------------
+// Create NAT Gateway for Private Subnet
+resource "aws_nat_gateway" "main" {
+  count         = length(var.public_subnet_cidrs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.cluster_name}-nat-${count.index + 1}"
+  }
+}
+
+
+// Create Elastic IP for NAT Gateway
+// count variable is used to create multiple EIPs. 
+// Dynamically determine the number of resources/instances to create based on the length of the public_subnet_cidrs variable.
+resource "aws_eip" "nat" { 
+  count = length(var.public_subnet_cidrs)
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.cluster_name}-nat-${count.index + 1}"
+  }
+}
+
+
+
+// Create Route table for private subnet 
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-private-${count.index + 1}"
+  }
+}
+
+
+// Associate Route Table with the Private Subnet
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
